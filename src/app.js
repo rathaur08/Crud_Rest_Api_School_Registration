1259b85ced5449998fd793a631430c2d52e21d39
Sunny <sunnyrathaur2444@gmail.com>
2023-01-04 13:14:53 +0530

    Add Async-Await to create a Students

--- src/app.js
+++ src/app.js
@@ -3,22 +3,21 @@ require("./db/conn");
 const Student = require("./models/students");
 
 const app = express();
-const port= process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 app.use(express.json());
 
 // app.get("/", (req, res) => {
 //     res.send("Hello From the Student Registration..");
 // });
 // Create a new Students --------------->
-app.post("/students", (req, res) => {
-    console.log(req.body);
-    const user = new Student(req.body);
-
-    user.save().than(() => {
-        res.status(201).send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
-    })
+app.post("/students", async (req, res) => {
+    try {
+        const user = new Student(req.body);
+        const CreateUser = await user.save();
+        res.status(201).send(CreateUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 })
 
 // Create HTTP Server Using Express
--- src/models/students.js
+++ src/models/students.js
@@ -1,40 +1,38 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-// require("../db/conn");
-
 
 // schema
 // A mongoose schema defines the structure of the document,
 // default value, validators, etc.,
-const studentSchema=new mongoose.Schema({
+const studentSchema = new mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         required: true,
         minlength: 3
     },
-    email:{
-        type:String,
-        required:true,
-        unique:[true, "Email id already Present"],
-        validite(value){ 
-            if(!validator.isEmail(value)){
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email id already Present"],
+        validite(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error("Email is inValid ");
             }
         }
     },
-    phone:{
-        type:Number,
-        min:10,
-        required:true,
-        unique:[true, "Phone number already Present"],
+    phone: {
+        type: Number,
+        min: 10,
+        required: true,
+        unique: [true, "Phone number already Present"],
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+        type: String,
+        required: true
     },
     active: Boolean,
     date: {
-        type:Date,
+        type: Date,
         default: Date.now
     }
 });
@@ -43,22 +41,4 @@ const studentSchema=new mongoose.Schema({
 // creation a new collection 
 const Student = new mongoose.model("Student", studentSchema);
 
-module.exports= Student;
-
-// Insert Data in MongoDB
-// const createDocument = async () =>{
-//     try {
-//         const reatUsers = new Student({
-//             name : "Sunny ",
-//             email: "sunny@",
-//             phone : "8266881450",
-//             address: "Faridnagar",
-//             active: true
-//         });
-//         const InsertData = await reatUsers.save();
-//         console.log(InsertData); 
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-// createDocument();
+module.exports = Student;
